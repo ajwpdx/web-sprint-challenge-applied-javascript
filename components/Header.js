84f0e55dc5a6617808b75f0b42e0be540cab4c99
Alex Williams <alexjwilliams1@gmail.com>
2020-07-10 08:35:57 -0700

    created the header in js

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,36 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const headerContainer = document.querySelector('div.header-container')
+console.log(Header())
+headerContainer.appendChild(Header())
+
+//header maker
+function Header() {
+    //creating elements
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
+
+
+    //assigning element attributes and content
+    header.className = 'header'
+    date.className = 'date'
+    temp.className = 'temp'
+
+    date.textContent = 'MARCH 28, 2020'
+    title.textContent = 'Lambda Times'
+    temp.textContent = '98°'
+
+
+    //setting up structure
+    header.appendChild(date)
+    header.appendChild(title)
+    header.appendChild(temp)
+
+    console.log(header)
+
+    return header
+
+}
